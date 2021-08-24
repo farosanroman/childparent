e7f16fb616c6a9a972285c50f7ff7b0fb21c8ddf
farosanroman <ppazpurua@gmail.com>
2021-08-24 03:22:55 -0400

    main

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hellossgaa {value}</div>;
+  return <div>Hellossgaabbb {value}</div>;
 }
 
 export default App;

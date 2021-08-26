6ad2900101780f6046763fad47d65b17356b77d5
farosanroman <ppazpurua@gmail.com>
2021-08-25 23:23:29 -0400

    ppa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hellogggaaa {value}</div>;
+  return <div>Helloggggaaa {value}</div>;
 }
 
 export default App;

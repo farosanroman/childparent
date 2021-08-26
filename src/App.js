bec5e7e0eb9c94e5e08b07dcb264219827baf765
farosanroman <ppazpurua@gmail.com>
2021-08-26 08:53:46 -0400

    qq

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hhhhhooooooo {value}</div>;
+  return <div>Static Webs {value}</div>;
 }
 
 export default App;
